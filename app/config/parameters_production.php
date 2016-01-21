9bf1b42f86a9c8cc54176e88414326a1d1e1a36c
julien <juliendeman.nc@gmail.com>
2016-01-21 22:33:17 +0100

    parameters_production

--- app/config/parameters_production.php
+++ app/config/parameters_production.php
@@ -3,11 +3,11 @@
     $db = parse_url(getenv('CLEARDB_DATABASE_URL'));
 
     $container->setParameter('database_driver', 'pdo_mysql');
-    $container->setParameter('database_host', 'localhost');//$db['host']);
-    $container->setParameter('database_port', '8889');//$db['port']);
-    $container->setParameter('database_name', 'symfony');//substr($db["path"], 1));
-    $container->setParameter('database_user', 'root');//$db['user']);
-    $container->setParameter('database_password', 'root');//$db['pass']);
+    $container->setParameter('database_host', $db['host']);//$db['host']);
+    $container->setParameter('database_port', $db['port']);//$db['port']);
+    $container->setParameter('database_name', substr($db["path"], 1));//substr($db["path"], 1));
+    $container->setParameter('database_user', $db['user']);//$db['user']);
+    $container->setParameter('database_password', $db['pass']);//$db['pass']);
     $container->setParameter('secret', getenv('SECRET'));
     $container->setParameter('locale', 'en');
     $container->setParameter('mailer_transport', 'gmail');
